ddaf7d76971cb0f2d08696db782f065c48d1a71c
shiftt <zhong@brae.co>
2020-06-05 19:59:59 +0800

    部分api向上层调用返回error

--- example/main.go
+++ example/main.go
@@ -11,8 +11,6 @@ import (
 
 func main() {
 
-	fmt.Println(reflect.TypeOf(model.Test{}).Name())
-
 	db.Setup(db.Conf{
 		Name:     "name",
 		User:     "root",
@@ -34,19 +32,20 @@ func main() {
 		testModel := new(model.Test)
 
 		// 通过selectById查询主键id为1的记录
-		a := sess.GetDao(testModel).SelectById(1, sorm.Load())
+		a, _ := sess.GetDao(testModel).SelectById(1, sorm.Load())
 		fmt.Println(a, a.(*model.Test).Name.Value())
 
 		sess.BeginTransaction()
 		// 在事务里面查询id为2的记录并加forupdate锁，调用Select方法会进行缓存，下次查询时直接返回缓存对象
-		b := sess.GetDao(testModel).Select(false, 2).Load(sorm.ForUpdate())
+		b, _ := sess.GetDao(testModel).Select(false, 2)
+		b.Load(sorm.ForUpdate())
 		fmt.Println(b, b.(*model.Test).Name.Value())
 		sess.SubmitTransaction()
 
 		testDao := sess.GetDao(testModel)
 
 		// 通过SelectOne查询单条记录
-		c := testDao.SelectOne(map[string]interface{}{
+		c, _ := testDao.SelectOne(map[string]interface{}{
 			"id": 3,
 		})
 		fmt.Println(c, c.(*model.Test).Name.Value())
@@ -69,5 +68,5 @@ func main() {
 	}, func(err exception.ErrorWrapper) {
 		fmt.Printf("%d  %s  %s\n", err.Code(), err.Error(), err.Position())
 		// 捕获未找到异常
-	}, model.TestNotFoundError)
+	}, model.TestNotFoundError, sorm.ModelRuntimeError)
 }
